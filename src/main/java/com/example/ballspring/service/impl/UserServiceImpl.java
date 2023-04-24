a9e8c0d1b72a93b7b21e91d80c1e8ef2b739f416
ggier9456 <ggier9456@gmail.com>
2023-04-24 17:48:23 +0800

    fifth

--- src/main/java/com/example/ballspring/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/ballspring/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.ballspring.model.User;
 import com.example.ballspring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 
 @Component
 public class UserServiceImpl implements UserService {
@@ -28,6 +29,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public String register(UserRegisterRequest userRegisterRequest) {
         if(userRegisterRequest.getPassword().equals(userRegisterRequest.getrPassword())){
+            String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+            userRegisterRequest.setPassword(hashedPassword);
             return userDao.register(userRegisterRequest);
         }else{
             return "輸入密碼與確認密碼不一致！";
@@ -36,6 +39,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User login(UserLoginRequest userLoginRequest) {
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+        userLoginRequest.setPassword(hashedPassword);
         User user = userDao.login(userLoginRequest);
         if(user == null){
             return null;
